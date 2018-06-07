63228770301b9273b383290a2d3cf1fe76f1fdf8
cchambe12 <cchamberlain@g.harvard.edu>
2018-06-07 08:47:00 -0400

    tweak to analysis script

--- analysis/TS_stan.R
+++ analysis/TS_stan.R
@@ -20,7 +20,7 @@ d<-read.csv("output/tree_rf_data.csv", header=TRUE)
 
 ## Budburst
 bb<-subset(d, phase=="budburst")
-bb<-dplyr::select(bb, doy, spp, photo, gdd, ID, year, aprecip, solr, chill, frz, tdiff, tmean, tmax, tmin, frost)
+bb<-dplyr::select(bb, doy, spp, photo, gdd, ID, year, aprecip, solr, chill, false.spring, tdiff, tmean, tmax, tmin, frost)
 
 bb$cgdd<-bb$gdd/20
 bb$cprecip<-bb$aprecip/20
@@ -39,32 +39,92 @@ bb.stan<-stan_glmer(doy~photo+cgdd+cprecip+cchill+
 bb.inter<-stan_glmer(doy~photo+cgdd+cchill+photo:cgdd+photo:cchill+cgdd:cchill+cprecip+
                        (1|spp), data=bb)
 
-bb.brm<-brm(doy~cgdd+photo+cprecip+csolr+(1|spp) + (cgdd-1|spp) +
-              (photo-1|spp) + (cprecip-1|spp) + (csolr-1|spp), data=bb)
+bb.brm<-brm(doy~cgdd+photo+cprecip+(1|spp) + (cgdd-1|spp) +
+              (photo-1|spp) + (cprecip-1|spp), data=bb)
+
+
+m<-bb.brm
+m.int<-posterior_interval(m)
+sum.m<-summary(m)
+cri.f<-as.data.frame(sum.m$fixed[,c("Estimate", "l-95% CI", "u-95% CI")])
+cri.f<-cri.f[-1,] #removing the intercept 
+fdf1<-as.data.frame(rbind(as.vector(cri.f[,1]), as.vector(cri.f[,2]), as.vector(cri.f[,3])))
+fdf2<-cbind(fdf1, c(0, 0, 0) , c("Estimate", "2.5%", "95%"))
+names(fdf2)<-c(rownames(cri.f), "spp", "perc")
+
+cri.r<-(ranef(m, summary = TRUE, robust = FALSE,
+              probs = c(0.025, 0.975)))$spp
+cri.r2<-cri.r[, ,-1]
+cri.r2<-cri.r2[,-2,]
+dims<-dim(cri.r2)
+twoDimMat <- matrix(cri.r2, prod(dims[1:2]), dims[3])
+mat2<-cbind(twoDimMat, c(rep(1:11, length.out=33)), rep(c("Estimate", "2.5%", "95%"), each=11))
+df<-as.data.frame(mat2)
+names(df)<-c(rownames(cri.f), "spp", "perc")
+dftot<-rbind(fdf2, df)
+dflong<- tidyr::gather(dftot, var, value, cgdd:`cprecip`, factor_key=TRUE)
+
+#adding the coef estiamtes to the random effect values 
+for (i in seq(from=1,to=nrow(dflong), by=36)) {
+  for (j in seq(from=3, to=25, by=1)) {
+    dflong$value[i+j]<- as.numeric(dflong$value[i+j]) + as.numeric(dflong$value[i])
+  }
+}
+dflong$rndm<-ifelse(dftot$spp>0, 2, 1)
+dfwide<-tidyr::spread(dflong, perc, value)
+dfwide[,4:6] <- as.data.frame(lapply(c(dfwide[,4:6]), as.numeric ))
+dfwide$spp<-as.factor(dfwide$spp)
+## plotting
+library(ggstance)
+pd <- position_dodgev(height = -0.5)
+
+estimates<-c("GDDs", "Photoperiod","Precipitation")
+dfwide$legend<-factor(dfwide$spp,
+                      labels=c("Overall Effects","Aesculus hippocastanum","Alnus glutinosa",
+                               "Betula pendula","Fagus sylvatica","Fraxinus excelsior",
+                               "Quercus robur"))
+estimates<-rev(estimates)
+#write.csv(dfwide, file="~/Documents/git/springfreeze/output/df_modforplot.csv", row.names=FALSE)
+fig1 <-ggplot(dfwide, aes(x=Estimate, y=var, color=spp, size=factor(rndm), alpha=factor(rndm)))+
+  geom_point(position =pd)+
+  geom_errorbarh(aes(xmin=(`2.5%`), xmax=(`95%`)), position=pd, size=.5, width=0)+
+  geom_vline(xintercept=0)+
+  scale_colour_manual(values=c("blue", "firebrick3", "firebrick1", "orangered1",
+                              "orange3", "orange1","sienna4", "sienna2", "green4", "purple2", "magenta3", "magenta1"))+
+  scale_size_manual(values=c(3, 2, 2, 2, 2, 2, 2, 2, 2, 2,2,2)) +
+  scale_shape_manual(labels="", values=c("1"=16,"2"=16))+
+  scale_alpha_manual(values=c(1, 0.5)) +
+  guides(size=FALSE, alpha=FALSE) +  
+  scale_y_discrete(limits = rev(unique(sort(dfwide$var))), labels=estimates) + ylab("") + 
+  labs(col="Effects") + theme(legend.box.background = element_rect(), 
+                              legend.title=element_blank(), legend.key=element_blank(),legend.key.size = unit(0.15, "cm"),
+                              legend.text=element_text(size=8), legend.position= c(0.78,0.88), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+                              panel.background = element_blank(), 
+                              axis.line = element_line(colour = "black")) #+
+#xlab(expression(atop("Model Estimate of Change ", paste("in Duration of Vegetative Risk (days)"))))
+quartz()
+fig1
+
+
 
 ## Leafout
 lo<-subset(d, phase=="leafout")
-lo<-dplyr::select(lo, doy, spp, photo, gdd, ID, year, aprecip, solr, chill, frz, tdiff, tmean, tmax, tmin, frost)
+lo<-dplyr::select(lo, doy, spp, photo, gdd, ID, year, aprecip, solr, chill, false.spring, tdiff, tmean, tmax, tmin, frost)
 
 lo<-lo[!duplicated(lo),]
 
 lo$cgdd<-lo$gdd/20
 lo$cprecip<-lo$aprecip/20
 lo$cchill<-lo$chill/50
-lo$cfrz<-lo$frz/20
 lo$csolr<-lo$solr/20
-lo$cyear<-as.numeric(substr(lo$year, 3,4))
 
-lo$doy<-ave(lo$doy, lo$ID, lo$year, FUN=first)
-#lo$resp<-ave(lo$doy, lo$spp, lo$year)
 
-#lo<-dplyr::select(lo, -ID)
-#lo<-lo[!duplicated(lo),]
-
-lo.stan<-stan_glmer(resp~photo+cgdd+cprecip+cchill+
+lo.stan<-stan_glmer(doy~cgdd+frost+cchill+cgdd:cchill+
                       (1|spp), data=lo)
 
-lo.stan<-stan_glmer(doy~cgdd+frost+cchill+cgdd:cchill+(1|spp), data=lo)
+#lo.stan<-stan_glmer(doy~cgdd+frost+cchill+cgdd:cchill+(1|spp), data=lo)
 
-lo.brm<-brm(resp~photo+cgdd+cprecip+cchill+(1|spp), data=lo)
+lo.brm<-brm(doy~cgdd+frost+cchill+cgdd:cchill+
+              (cgdd-1|spp) + (frost-1|spp) +
+              (cgdd-1|spp) + (1|spp), data=lo)
 
