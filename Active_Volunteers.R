e21cd0a96cfed054d79a8fcce619f833416168de
cchambe12 <cchamberlain@g.harvard.edu>
2020-07-07 11:56:48 -0400

    arb annual report new  numbers

--- Active_Volunteers.R
+++ Active_Volunteers.R
@@ -115,7 +115,8 @@ dx.r<-inner_join(dx.r, xx)
 
 ####### Stop here Jun 19, 2018 - issues with mapping budburst date #########
 dx.r$code<-reorder(dx.r$species, dx.r$mean)
-cols <- colorRampPalette(brewer.pal(4, "Dark2"))(4)
+cols <- colorRampPalette(brewer.pal(5, "Dark2"))(5)
+colz <- c("salmon3", "royalblue3")
 dx.r$colz <-ifelse(dx.r$pheno=="Breaking leaf buds", "salmon3", "royalblue3")
 dvr2016<-ggplot(dx.r[(dx.r$year==2016),], aes(x=code, y=mean)) + geom_point(aes(color=rev(colz),shape=pheno)) +
   geom_line(col="green4", alpha=0.3) + ylab("Day of Year") +theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
@@ -162,22 +163,37 @@ dvr2018<-ggplot(dx.r[(dx.r$year==2018),], aes(x=code, y=mean)) + geom_point(aes(
   scale_y_continuous(breaks=seq(min(100), max(140), by=10))
 
 dvr2019<-ggplot(dx.r[(dx.r$year==2019),], aes(x=code, y=mean)) + geom_point(aes(color=rev(colz),shape=pheno)) +
-  geom_line(col="green4", alpha=0.3) + coord_flip(ylim=c(100,140)) + ylab("Day of Year") + xlab("Species") +theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
-                                                                                                                  axis.text.y = element_blank(),
-                                                                                                                  axis.title.y = element_blank(),
-                                                                                                                  axis.ticks.y = element_blank(),
-                                                                                                                  axis.text=element_text(size=9), legend.key = element_rect(fill = "transparent"),
-                                                                                                                  legend.box.background = element_rect(),
-                                                                                                                  plot.title = element_text(color="#E7298A"),
-                                                                                                                  legend.text = element_text(size=7),
-                                                                                                                  legend.title = element_text(size=8)) + labs(col="Phenophase") + 
+  geom_line(col="green4", alpha=0.3) + coord_flip(ylim=c(100,140)) + ylab("Day of Year") + xlab("Species") +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
+  axis.text.y = element_blank(),
+  axis.title.y = element_blank(), legend.position = "none",
+  axis.ticks.y = element_blank(),
+  axis.text=element_text(size=9), legend.key = element_rect(fill = "transparent"),
+  legend.box.background = element_rect(),
+  plot.title = element_text(color="#E7298A"),
+  legend.text = element_text(size=7),
+  legend.title = element_text(size=8)) + labs(col="Phenophase") + 
   geom_hline(aes(yintercept=bb.yr), xx[(xx$year==2019),], col="black", linetype="dashed") +
   scale_shape_manual(name="Phenophase", values=c(16, 17), labels=c("Budburst", "Leafout")) +
   scale_color_manual(name="Phenophase", values=dx.r$colz, labels=c("Budburst", "Leafout")) + ggtitle("2019") +
   scale_y_continuous(breaks=seq(min(100), max(140), by=10))
 
+dvr2020<-ggplot(dx.r[(dx.r$year==2020),], aes(x=code, y=mean)) + geom_point(aes(color=rev(colz),shape=pheno)) +
+  geom_line(col="green4", alpha=0.3) + ylab("Day of Year") +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks.y = element_blank(), legend.key = element_rect(fill = "transparent"),
+        legend.box.background = element_rect(),
+        panel.spacing = unit(2, "lines"),
+        plot.title = element_text(color=cols[5])) + labs(col="Phenophase") + 
+  geom_hline(aes(yintercept=bb.yr), xx[(xx$year==2020),], col="black", linetype="dashed") +
+  scale_shape_manual(name="Phenophase", values=c(16, 17), labels=c("Budburst", "Leafout")) +
+  scale_color_manual(name="Phenophase", values=rev(colz), labels=c("Budburst", "Leafout")) + ggtitle("2020") +
+  scale_y_continuous(breaks=seq(min(100), max(140), by=10)) + coord_flip(ylim=c(100,140)) 
 
-allyrs <- ggarrange(dvr2016, dvr2017, dvr2018, dvr2019,  ncol=4)
+
+allyrs <- ggarrange(dvr2016, dvr2017, dvr2018, dvr2019, dvr2020,  ncol=5)
 
 
 ### Let's add in Climate data now
@@ -194,8 +210,8 @@ spring <- clim[(clim$doy>=1 & clim$doy<=140),]
 
 climate <- ggplot(spring, aes(x=doy, y=tmean, col=as.factor(year))) + #geom_point(aes(col=as.factor(year)), alpha=0.1) +
   geom_smooth(aes(col=as.factor(year), fill=as.factor(year)), stat="smooth", method="loess", se=TRUE, span=0.9) + 
-  scale_color_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019")) +
-   scale_fill_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019")) +
+  scale_color_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+   scale_fill_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
   theme_classic() + xlab("Day of Year") + ylab("Mean \n Temperature (°C)") +
   coord_cartesian(ylim=c(-8, 18), expand=0) + scale_x_continuous(breaks = seq(min(0), max(140), by=30)) +
   scale_y_continuous(breaks=seq(min(-8), max(18), by=4)) + theme(panel.spacing = unit(c(0,0,5,5),"cm"),
@@ -203,11 +219,72 @@ climate <- ggplot(spring, aes(x=doy, y=tmean, col=as.factor(year))) + #geom_poin
                                                                  legend.title = element_text(size=8),
                                                                  legend.key.size = unit(0.8,"line"))
 
+clim2016 <- ggplot(spring[(spring$year==2016),], aes(x=doy, y=tmean), col=cols[1]) + #geom_point(aes(col=as.factor(year)), alpha=0.1) +
+  geom_smooth(col=cols[1], fill=cols[1], stat="smooth", method="loess", se=TRUE, span=0.9) + 
+  #scale_color_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  #scale_fill_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  theme_classic() + xlab("Day of Year") + ylab("Mean \n Temperature (°C)") +
+  coord_cartesian(ylim=c(-8, 18), expand=0) + scale_x_continuous(breaks = seq(min(0), max(140), by=30)) +
+  scale_y_continuous(breaks=seq(min(-8), max(18), by=4)) + theme(panel.spacing = unit(c(0,0,5,5),"cm"),
+                                                                 legend.text = element_text(size=7),
+                                                                 legend.title = element_text(size=8),
+                                                                 legend.key.size = unit(0.8,"line"),
+                                                                 legend.position = "none")
+clim2017 <- ggplot(spring[(spring$year==2017),], aes(x=doy, y=tmean), col=cols[2]) + #geom_point(aes(col=as.factor(year)), alpha=0.1) +
+  geom_smooth(col=cols[2], fill=cols[2], stat="smooth", method="loess", se=TRUE, span=0.9) + 
+  #scale_color_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  #scale_fill_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  theme_classic() + xlab("Day of Year") + ylab("Mean \n Temperature (°C)") +
+  coord_cartesian(ylim=c(-8, 18), expand=0) + scale_x_continuous(breaks = seq(min(0), max(140), by=30)) +
+  scale_y_continuous(breaks=seq(min(-8), max(18), by=4)) + theme(panel.spacing = unit(c(0,0,5,5),"cm"),
+                                                                 legend.text = element_text(size=7),
+                                                                 legend.title = element_text(size=8),
+                                                                 legend.key.size = unit(0.8,"line"),
+                                                                 legend.position = "none")
+
+clim2018 <- ggplot(spring[(spring$year==2018),], aes(x=doy, y=tmean), col=cols[3]) + #geom_point(aes(col=as.factor(year)), alpha=0.1) +
+  geom_smooth(col=cols[3], fill=cols[3], stat="smooth", method="loess", se=TRUE, span=0.9) + 
+  #scale_color_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  #scale_fill_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  theme_classic() + xlab("Day of Year") + ylab("Mean \n Temperature (°C)") +
+  coord_cartesian(ylim=c(-8, 18), expand=0) + scale_x_continuous(breaks = seq(min(0), max(140), by=30)) +
+  scale_y_continuous(breaks=seq(min(-8), max(18), by=4)) + theme(panel.spacing = unit(c(0,0,5,5),"cm"),
+                                                                 legend.text = element_text(size=7),
+                                                                 legend.title = element_text(size=8),
+                                                                 legend.key.size = unit(0.8,"line"),
+                                                                 legend.position = "none")
+
+clim2019 <- ggplot(spring[(spring$year==2019),], aes(x=doy, y=tmean), col=cols[4]) + #geom_point(aes(col=as.factor(year)), alpha=0.1) +
+  geom_smooth(col=cols[4], fill=cols[4], stat="smooth", method="loess", se=TRUE, span=0.9) + 
+  #scale_color_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  #scale_fill_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  theme_classic() + xlab("Day of Year") + ylab("Mean \n Temperature (°C)") +
+  coord_cartesian(ylim=c(-8, 18), expand=0) + scale_x_continuous(breaks = seq(min(0), max(140), by=30)) +
+  scale_y_continuous(breaks=seq(min(-8), max(18), by=4)) + theme(panel.spacing = unit(c(0,0,5,5),"cm"),
+                                                                 legend.text = element_text(size=7),
+                                                                 legend.title = element_text(size=8),
+                                                                 legend.key.size = unit(0.8,"line"),
+                                                                 legend.position = "none")
+
+clim2020 <- ggplot(spring[(spring$year==2020),], aes(x=doy, y=tmean), col=cols[5]) + #geom_point(aes(col=as.factor(year)), alpha=0.1) +
+  geom_smooth(col=cols[5], fill=cols[5], stat="smooth", method="loess", se=TRUE, span=0.9) + 
+  #scale_color_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  #scale_fill_manual(name = "Year", values=cols, labels = c("2016", "2017", "2018", "2019", "2020")) +
+  theme_classic() + xlab("Day of Year") + ylab("Mean \n Temperature (°C)") +
+  coord_cartesian(ylim=c(-8, 18), expand=0) + scale_x_continuous(breaks = seq(min(0), max(140), by=30)) +
+  scale_y_continuous(breaks=seq(min(-8), max(18), by=4)) + theme(panel.spacing = unit(c(0,0,5,5),"cm"),
+                                                                 legend.text = element_text(size=7),
+                                                                 legend.title = element_text(size=8),
+                                                                 legend.key.size = unit(0.8,"line"),
+                                                                 legend.position = "none")
+
+climall <- ggarrange(clim2016, clim2017, clim2018, clim2019, clim2020,  ncol=5)
+
 
 quartz()
-grid.arrange(allyrs, climate, nrow=3, heights = c(3, 0.5, 1.3), layout_matrix=rbind(c(1, 1, 1, 1),
+grid.arrange(allyrs, climall, nrow=3, heights = c(3, 0.5, 1.5), layout_matrix=rbind(c(1, 1, 1, 1, 1),
                                                                              c(NA),
-                                                                             c(NA, 2, 2, 2, NA)))
+                                                                             c(NA, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, NA)))
 
 
 dx.h<-filter(dx, pheno!="Breaking leaf buds")
@@ -390,7 +467,7 @@ dnine<-df %>%
   mutate(ObservedBy_Person_ID = strsplit(as.character(ObservedBy_Person_ID), ",")) %>% 
   unnest(ObservedBy_Person_ID)
 
-total.numobs.nine <- as.data.frame(table(sort(deight$ObservedBy_Person_ID)))
+total.numobs.nine <- as.data.frame(table(sort(dnine$ObservedBy_Person_ID)))
 total.numobs.nine <- data.frame(observer=total.numobs.nine$Var1, n=total.numobs.nine$Freq)
 numobs.nine <- subset(total.numobs.nine, total.numobs.nine$n>40) ## 35 Observers
 
@@ -518,12 +595,14 @@ hist2018<-ggplot(total.numobs.eight, aes(x=n)) + geom_histogram(binwidth = 11, s
 
 
 library(viridis)
-cols <- viridis_pal(option="D")(63)
+cols <- viridis_pal(option="C")(63)
 histall<-ggplot(allobsers, aes(x=totalobs)) + geom_histogram(binwidth = 20, size=0.6, fill=cols) +
   xlab("Number of Observations") + ylab("Number of Observers") + coord_cartesian(expand=c(0,0)) +
   theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), 
         legend.position = c(0.05,0.85), legend.text = element_text(size=8), legend.key.size = unit(0.5, "cm"),
-        axis.title=element_text(size=12), legend.title = element_text(size=8), axis.text=element_text(size=10))
+        axis.title=element_text(size=12), legend.title = element_text(size=8), axis.text=element_text(size=10),
+        plot.title=element_text(face="bold")) +
+  ggtitle("Number of observations per observer") + labs(caption="*Note: colors do not signify anything")
 
 quartz()
 ggarrange(hist2016, hist2017, hist2018, ncol=3)
--- numberandfigures2019.R
+++ numberandfigures2019.R
@@ -12,6 +12,7 @@ library(tidyr)
 library(ggplot2)
 library(RColorBrewer)
 library(egg)
+library(viridis)
 
 # Set Working Directory
 setwd("~/Documents/git/treespotters/analysis/input")
@@ -30,7 +31,8 @@ getPalette = colorRampPalette(brewer.pal(8, "Spectral"))
 
 sppnum <- ggplot(numobsperspp, aes(x=spp, y=n, fill=spp)) + geom_bar(stat="identity", position=position_dodge()) +
   scale_fill_manual(values=getPalette(colourCount), 
-                    labels=numobsperspp$spp) +
+                    labels=numobsperspp$spp) + 
+  labs(title="Total number of observed phenophases \n(e.g., leaves or flowers) for each species (2015-2019).") +
   scale_x_discrete(labels=c("Acer rubrum" = "Red Maple",
                             "Acer saccharum" = "Sugar Maple",
                             "Aesculus flava" = "Yellow Buckeye",
@@ -47,8 +49,9 @@ sppnum <- ggplot(numobsperspp, aes(x=spp, y=n, fill=spp)) + geom_bar(stat="ident
                             "Vaccinium corymbosum" = "Highbush Blueberry",
                             "Viburnum nudum" = "Possumhaw")) +
   theme_classic() + theme(legend.position="none", 
-                          plot.margin=unit(c(1,3,1,1), "lines")) +
-  ylab("Number of 'Yes' observations") + xlab("") +
+                          plot.margin=unit(c(1,3,1,1), "lines"),
+                          plot.title = element_text(face="bold")) +
+  ylab("Number of observed phenophases") + xlab("") +
   coord_flip(expand=c(0,0), clip="off") + 
   geom_text(aes(label=n), hjust=-0.2, col=getPalette(colourCount))
 
@@ -63,18 +66,22 @@ df$obssum <- c(12524, 12524+53502, 12524+53502+66218, 12524+53502+66218+60971, 1
 cols<-colorRampPalette(brewer.pal(8, "Dark2"))(5)
 colpal <- colorRampPalette(c("blue3", "red3"))
 colvir <- viridis_pal(option="C")(6)
+quartz()
 ggplot(df, aes(x=year, y=obssum)) + geom_line(aes(col=as.factor(obssum)), col=colpal(5)) + geom_point(aes(col=as.factor(year))) + 
   theme_classic() + scale_color_manual(values=colpal(5), labels=df$year) + 
   theme(legend.position = "none",
         plot.margin = unit(c(1,4.5,1,1), "lines"),
-        axis.text.x=element_text(color=colpal(5))) +
+        plot.title=element_text(face="bold"),
+        axis.text.x=element_text(color=colpal(5))) + ggtitle("Total number of observations over time") +
   ylab("Total Number of Observations") +
   geom_text(aes(label=obssum, col=as.factor(year), size=as.factor(year)),hjust=-0.2, vjust=0.5) + xlab("") +
   coord_cartesian(clip = 'off')
+  
+
 
 ### Pie Chart
 routes <- data.frame(route=c("Linden and North Woods", "Maple", "Shrub", "Birch", "Oak",
-                             "Hickory", "Beech", "Peter's Hill"), 
+                             "Hickory", "Beech", "Peters Hill"), 
                      n=c(23027, 22861, 11135, 57517, 22293, 36661, 12838, 53795))
 
 
@@ -82,11 +89,12 @@ routes <- data.frame(route=c("Linden and North Woods", "Maple", "Shrub", "Birch"
 piechart <- ggplot(routes, aes(x="", y=n, fill=route))+
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y", start=0) + theme_classic() +
-  scale_fill_brewer(palette="Dark2", name="Route") +
+  scale_fill_brewer(palette="Dark2", name="Route") + ggtitle("Percentage of total observations by route") +
   theme(axis.line = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
-        axis.title = element_blank()) +
+        axis.title = element_blank(),
+        plot.title=element_text(face="bold")) +
   geom_text(aes(label = paste0(round((n/239710)*100), "%")), position = position_stack(vjust = 0.5))
   
 quartz()
